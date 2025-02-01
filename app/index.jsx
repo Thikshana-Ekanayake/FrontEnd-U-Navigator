95ae4a1b44ebb85da6e93791e50d59a9e073f803
Thikshana Ekanayake <ekanayakeemtl.21@uom.lk>
2025-02-01 20:51:08 +0530

    Basic Structure

---
+++ app/index.jsx
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import "../global.css";
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className = "text-3xl">U Navigator</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // NOTE: Update this to include the paths to all of your component files.
+  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      colors: {
+        dark: "#03045E",
+        text: "#176FF2",
+        primary: {
+          g1: "#0047B2",
+          g2: "#25A8F5",
+        },
+        white: "#FFFFFF",
+        smoke: "#B8B8B8",
+        accent: "#FFC24D",
+      },
+      fontFamily: {
+        roboto: ["Roboto", "sans-serif"],
+        lato: ["Lato", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+}
